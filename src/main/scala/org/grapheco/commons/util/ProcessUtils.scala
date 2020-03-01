e5193139185d4d8f02349224fca50f8fb1a4f24f
bluejoe2008@gmail.com <bluejoe2008@gmail.com>
2020-03-01 22:03:41 +0800

    initial

---
+++ src/main/scala/org/grapheco/commons/util/ProcessUtils.scala
@@ -0,0 +1,13 @@
+package org.grapheco.commons.util
+
+import java.lang.management.ManagementFactory
+
+/**
+  * Created by bluejoe on 2020/2/25.
+  */
+object ProcessUtils {
+  def getCurrentPid(): Int = {
+    val runtimeMXBean = ManagementFactory.getRuntimeMXBean();
+    return runtimeMXBean.getName().split("@")(0).toInt;
+  }
+}
